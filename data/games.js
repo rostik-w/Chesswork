79740153be771c40834249c8427800e2979f9ae7
Orion Auld <orion@swiftstack.com>
2015-06-04 19:58:53 -0700

    Play through games on SwiftStack community board
    Separated game datafile from pgn viewer code
    Added SwiftStack game data
    Cleaned up headers and the shown metadata
    Made favicons

---
+++ data/games.js
@@ -0,0 +1,32 @@
+pgnData = [
+  [
+    '[Event "SwiftStack Ongoing Play"]',
+    '[Site "SwiftStack HQ"]',
+    '[Date "2015.5.26"]',
+    '[EventDate "2015.5.26"]',
+    '[White "Ed McClanahan et al."]',
+    '[Black "Alexander Corwin et al."]',
+    '',
+    '1. e4 c6 2. Nf3 d5 3. Nc3 dxe4 4. Nxe4 Bg4 5. h3 Bxf3 6. Qxf3 e6 7. Bc4 Nd7'
+  ],
+  [
+    '[Event "SwiftStack Ongoing Play"]',
+    '[Site "SwiftStack HQ"]',
+    '[Date "2015.5.15"]',
+    '[EventDate "2015.5.15"]',
+    '[White "Clay Gerrard et al."]',
+    '[Black "Orion Auld et al."]',
+    '',
+    '1. e4 c6 2. Nf3 d5 3. exd5 cxd5 4. Ne5 Nd7 5. d4 Nxe5 6. dxe5 e6 7. Bd3 Bc5 8. O-O Ne7 9. Nd2 O-O 10. Qh5 Ng6 11. Nf3 Qc7 12. Ng5 h6 13. Nxf7 Qxf7 14. Bxg6 Qxf2+ 15. Rxf2 Rxf2 16. Bg5 Rf5+ 17. Kh1 Rxg5 18. Bf7+ Kh8 19. Qe2 Bd7 20. c3 Bc6 21. Bxe6 Re8 22. h4 Rg3 23. Kh2 Re3 24. Qg4 Rxe5 25. Bf7 Re4 26. Qg3 Rd8 27. Kh1 Bd6 28. Qh3 Bd7 29. g4 Rxg4 30. Qf3 Rxh4+ 31. Kg1 Rh3 32. Qf2 Rg3+ 33. Kh1 Rf8 34. Rd1 Rg5 35. Rd3 Bf5 36. Bxd5 0-1'
+  ],
+  [
+    '[Event "SwiftStack Ongoing Play"]',
+    '[Site "SwiftStack HQ"]',
+    '[Date "2015.5.02"]',
+    '[EventDate "2015.5.02"]',
+    '[White "Orion Auld et al."]',
+    '[Black "Alexander Corwin et al."]',
+    '',
+    '1. d4 d5 2. c4 dxc4 3. Nc3 Nf6 4. e3 a6 5. Bxc4 e6 6. Nf3 b5 7. Bb3 Bd6 8. e4 b4 9. e5 bxc3 10. bxc3 Bb7 11. O‑O O‑O 12. exd6 cxd6 13. Ng5 Qa5 14. Qd3 h6 15. Nf3 Be4 16. Qd2 Nc6 17. Bb2 e5 18. Rac1 Bxf3 19. gxf3 Nxd4 20. Qe3 Nxb3 21. axb3 Qa2 22. Rb1 Qxb3 23. Ba1 Qc2 24. Rb2 Qg6+ 25. Kh1 Nd5 26. Qd2 Nf4 27. Rg1 Qh5 28. Rg3 Rfc8 29. Rb1 Qf5 30. Re1 Nh5 31. Rg2 Qxf3 32. Re3 Qb7 33. f3 Nf4 34. Rg4 Rc6 35. Rg1 Rb6 36. Qc2 Rb8 37. Qe4 Qc8 38. Ree1 a5 39. Rd1 Qc5 40. Rgf1 f6 41. Qf5 d5 42. Rg1 R6b7 43. Rdf1 Qb5 44. c4 Qxc4 45. Re1 Qb3 46. Rgf1 a4 47. Bd4 Qd3 48. Qxd3 Nxd3 49. Rd1 Rb3 50. Bg1 a3 51. Ra1 Ra8 52. Rfd1 1-0'
+  ],
+];
